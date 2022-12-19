f413ce915a198263d48909427e4d6fc733254f18
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2022-12-19 13:24:29 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+
+const image = { uri: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2783&q=80" };
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={image} style={styles.image}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      </ImageBackground>
     </View>
   );
 }
@@ -17,4 +22,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: "100%",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text: {
+    fontSize: 25,
+    fontWeight: '500',
+    color: 'white'
+  }
 });
