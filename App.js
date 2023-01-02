1af0de6e673da87749edd2400251ab4dd57c359f
sourav.kumar.pany <sourav.kumar.pany@gds.ey.com>
2023-01-02 21:02:04 +0530

    Added Fetch API both in mobile and web

--- App.js
+++ App.js
@@ -3,10 +3,43 @@ import Navigation from './navigation/navigation';
 import Home from './pages/home/Home';
 import { StatusBar } from 'expo-status-bar';
 import { value } from './redux/Header';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, FlatList, ActivityIndicator, View, StyleSheet } from 'react-native';
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
+import React, { useEffect, useState } from 'react';
+
 export default function App() {
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch('http://jsonplaceholder.typicode.com/posts');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  // Flat list item component
+  const ItemView = ({ item }) => {
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.item}>{item.id}</Text>
+        <Text style={styles.item}>{item.title}</Text>
+      </View>
+    );
+  };
+
 
   return (
     <>
@@ -14,6 +47,10 @@ export default function App() {
       <SafeAreaView>
         <Text>Hello Mobile App {value}</Text>
         <Calendar />
+        {isLoading ? <ActivityIndicator /> : (
+          <FlatList data={data} renderItem={ItemView} keyExtractor={(item) => item.id} />
+        )}
+
       </SafeAreaView>
       {/*
       <Navigation /> */}
@@ -21,3 +58,23 @@ export default function App() {
     </>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    marginBottom: 10,
+    marginTop: 30,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+  },
+});
+
--- src/web/Home.js
+++ src/web/Home.js
@@ -2,12 +2,38 @@ import React from "react";
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import { value } from "../redux/Header";
+import { useEffect, useState } from 'react';
 
 const Home = () => {
+
+  const [data, setData] = useState([]);
+
+
+  const getPost = async () => {
+    try {
+      const response = await fetch('http://jsonplaceholder.typicode.com/posts');
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.error("Loading false");
+    }
+  }
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
   return (
     <div>
-      <Calendar />
       <h2 style={{ color: "#9400d3", fontSize: "32" }}>Hello Web App {value}</h2>
+      <Calendar />
+
+      {data.map((item) => {
+        return <h2 style={{ fontSize: "20" }} key={(item) => item.id}>{item.id} {item.title}</h2>
+      })}
+
     </div>
   );
 };
